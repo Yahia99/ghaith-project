4ace2d31e6f027bb6d13bf5b52f406207ff0cd14
Yahia <78263823+Yahia99@users.noreply.github.com>
2024-07-10 14:35:23 +0200

    .

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,119 +1,206 @@
 import * as React from "react";
 import { useState } from "react";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import Masonry from "@mui/lab/Masonry";
-import { styled } from "@mui/material/styles";
-
-const Label = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(0.5),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-  borderBottomLeftRadius: 0,
-  borderBottomRightRadius: 0,
-}));
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import ImageListItemBar from "@mui/material/ImageListItemBar";
+import ListSubheader from "@mui/material/ListSubheader";
+import IconButton from "@mui/material/IconButton";
+import InfoIcon from "@mui/icons-material/Info";
+import Container from "@mui/material/Container";
+import { useMediaQuery, useTheme } from "@mui/material";
 
 function Gallery() {
-  const [showLabel, setShowLabel] = useState<boolean>(false);
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const [showItemBar, setShowItemBar] = useState<boolean>(false);
+  const [counter, setCounter] = useState<{ counter: number }>({ counter: 0 });
+
   return (
-    <Box sx={{ minWidth: 500, height: "100%", padding: 8 }}>
-      <Box sx={{ minWidth: 500, minHeight: 829 }}>
-        <Masonry columns={4} spacing={3}>
-          {itemData.map((item, index) => (
-            <div key={index}>
-              {showLabel ? <Label>{index + 1}</Label> : ""}
-              <img
-                srcSet={`${item.img}?w=162&auto=format&dpr=2 2x`}
-                src={`${item.img}?w=162&auto=format`}
-                alt={item.title}
-                loading="lazy"
-                style={{
-                  borderBottomLeftRadius: 4,
-                  borderBottomRightRadius: 4,
-                  display: "block",
-                  width: "100%",
-                }}
-                onMouseEnter={() => setShowLabel(true)}
-                onMouseLeave={() => setShowLabel(false)}
+    <Container maxWidth="lg">
+      <ImageList gap={20}>
+        <ImageListItem key="Subheader" cols={2}>
+          <ListSubheader component="div">December</ListSubheader>
+        </ImageListItem>
+        {itemData.map((item, index) => (
+          <ImageListItem
+            key={item.img}
+            onMouseEnter={() => {
+              setShowItemBar(true);
+              setCounter({ ...counter, counter: index });
+            }}
+            onMouseLeave={() => setShowItemBar(false)}
+          >
+            <img
+              srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              src={`${item.img}?w=248&fit=crop&auto=format`}
+              alt={item.title}
+              loading="lazy"
+            />
+            {showItemBar && index === counter.counter ? (
+              <ImageListItemBar
+                title={item.title}
+                subtitle={item.author}
+                actionIcon={
+                  <IconButton
+                    sx={{ color: "rgba(255, 255, 255, 0.54)" }}
+                    aria-label={`info about ${item.title}`}
+                  >
+                    <InfoIcon />
+                  </IconButton>
+                }
               />
-            </div>
-          ))}
-        </Masonry>
-      </Box>
-    </Box>
+            ) : (
+              ""
+            )}
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </Container>
   );
 }
 
 const itemData = [
   {
-    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
-    title: "Fern",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1627308595229-7830a5c91f9f",
-    title: "Snacks",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
-    title: "Mushrooms",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1529655683826-aba9b3e77383",
-    title: "Tower",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
-    title: "Sea star",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
-    title: "Honey",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
-    title: "Basketball",
-  },
-  {
     img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
     title: "Breakfast",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1627328715728-7bcc1b5db87d",
-    title: "Tree",
+    author: "@bkristastucchio",
+    rows: 2,
+    cols: 2,
+    featured: true,
   },
   {
     img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
     title: "Burger",
+    author: "@rollelflex_graphy726",
   },
   {
     img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
     title: "Camera",
+    author: "@helloimnik",
   },
   {
     img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
     title: "Coffee",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1627000086207-76eabf23aa2e",
-    title: "Camping Car",
+    author: "@nolanissac",
+    cols: 2,
   },
   {
     img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
     title: "Hats",
+    author: "@hjrc33",
+    cols: 2,
+  },
+  {
+    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+    title: "Honey",
+    author: "@arwinneil",
+    rows: 2,
+    cols: 2,
+    featured: true,
+  },
+  {
+    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+    title: "Basketball",
+    author: "@tjdragotta",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+    title: "Fern",
+    author: "@katie_wasserman",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+    title: "Mushrooms",
+    author: "@silverdalex",
+    rows: 2,
+    cols: 2,
   },
   {
     img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
     title: "Tomato basil",
+    author: "@shelleypauls",
   },
   {
-    img: "https://images.unsplash.com/photo-1627328561499-a3584d4ee4f7",
-    title: "Mountain",
+    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+    title: "Sea star",
+    author: "@peterlaster",
   },
   {
     img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
     title: "Bike",
+    author: "@southside_customs",
+    cols: 2,
   },
 ];
+
+// const itemData = [
+//   {
+//     img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+//     title: "Fern",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1627308595229-7830a5c91f9f",
+//     title: "Snacks",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+//     title: "Mushrooms",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1529655683826-aba9b3e77383",
+//     title: "Tower",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+//     title: "Sea star",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+//     title: "Honey",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+//     title: "Basketball",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+//     title: "Breakfast",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1627328715728-7bcc1b5db87d",
+//     title: "Tree",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+//     title: "Burger",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+//     title: "Camera",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+//     title: "Coffee",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1627000086207-76eabf23aa2e",
+//     title: "Camping Car",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+//     title: "Hats",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+//     title: "Tomato basil",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1627328561499-a3584d4ee4f7",
+//     title: "Mountain",
+//   },
+//   {
+//     img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
+//     title: "Bike",
+//   },
+// ];
 export default Gallery;
