6ee9810668013827fcc66d33eb37f1e797e48785
Tahsin <yahia.tahsin@keuco.de>
2024-07-16 16:52:08 +0200

    .

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,28 +1,36 @@
 import React, { useState } from "react";
-import Box from "@mui/material/Box";
-import { AppBar, IconButton, Toolbar } from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import { Box, Container, Typography } from "@mui/material";
 
-export default function LandingPage() {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const open = (newOpen: boolean) => {
-    setIsOpen(!newOpen);
-  };
+function LandingPage() {
+  const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false);
+  const trigger = useScrollTrigger({ disableHysteresis: true, threshold: 0 });
   return (
-    <Box sx={{ height: "100vh", bgcolor: "#ccc" }}>
-      <AppBar component="nav" position="fixed">
+    <>
+      <AppBar
+        position="sticky"
+        sx={trigger ? {} : { bgcolor: "#ccc" }}
+        elevation={trigger ? 4 : 0}
+      >
         <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={() => open(isOpen)}
-            sx={{ mr: 2, display: { sm: "none" } }}
-          >
+          <IconButton color="inherit" aria-label="menu" sx={{ mr: 2 }}>
             <MenuIcon />
           </IconButton>
+          <Typography variant="h6" component="div" noWrap sx={{ flexGrow: 1 }}>
+            Ghaith Fineart
+          </Typography>
         </Toolbar>
       </AppBar>
-    </Box>
+      <Container sx={{ height: "calc(100vh - 56px)", bgcolor: "#ccc" }}>
+        <Typography>setTest</Typography>
+        <img src="../s.svg" alt="#" />
+      </Container>
+    </>
   );
 }
+
+export default LandingPage;
